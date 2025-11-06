555dcaa3e1eb27c47bae018104767ce217f06770
axunion <120321953+axunion@users.noreply.github.com>
2025-11-06 23:05:42 +0900

    chore: migrate package manager to pnpm

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -8,7 +8,7 @@ interface MainLayoutProps {
 export function MainLayout(props: MainLayoutProps) {
 	return (
 		<div class="min-h-screen bg-slate-50">
-			<div class="max-w-md md:mx-auto bg-white p-6 border-x border-slate-200 shadow-md">
+			<div class="max-w-md mx-auto bg-white p-6 border-x border-slate-200 shadow-md">
 				{props.title && (
 					<header class="mb-6">
 						<h1 class="font-['Bonheur_Royale',cursive] text-3xl text-center text-sky-500">
